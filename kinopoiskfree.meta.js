f722badd227b9995ec69dedf2d10ababfd12e96a
LexKreyn <48969431+LexKreyn@users.noreply.github.com>
2024-05-20 10:25:26 +0400

    First commit

---
+++ kinopoiskfree.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name           KinopoiskFree
+// @name:ru        Бесплатный Кинопоиск
+// @namespace      http://tampermonkey.net/
+// @version        1.4
+// @description    Add button for free watch
+// @description:ru Добавляет кнопку для бесплатного просмотра
+// @author         Lex
+// @copyright      2024, Lex
+// @icon           https://www.kinopoisk.ru/favicon.ico
+// @icon64         https://www.kinopoisk.ru/favicon.ico
+// @homepage       https://www.kinopoisk.ru/
+// @match          https://*.kinopoisk.ru/*
+// @grant          none
+// @run-at         document-end
+// @license        MIT
+// @updateURL      https://raw.githubusercontent.com/LexKreyn/KinopoiskFree/main/kinopoiskfree.meta.js
+// @downloadURL    https://raw.githubusercontent.com/LexKreyn/KinopoiskFree/main/kinopoiskfree.user.js
+// ==/UserScript==
