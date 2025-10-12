fd7e2b890cff402644f231fd3a8fbd3fd527055a
LexKreyn <48969431+LexKreyn@users.noreply.github.com>
2025-10-12 12:30:53 +0400

    Update kinopoiskfree.meta.js

--- kinopoiskfree.meta.js
+++ kinopoiskfree.meta.js
@@ -2,7 +2,7 @@
 // @name           KinopoiskFree
 // @name:ru        Бесплатный Кинопоиск
 // @namespace      http://tampermonkey.net/
-// @version        1.11
+// @version        1.12
 // @description    Add modal with links for free watch
 // @description:ru Добавляет окно с ссылками для бесплатного просмотра
 // @author         Lex
@@ -19,3 +19,4 @@
 // @homepageURL    https://github.com/LexKreyn/KinopoiskFree#readme
 // @supportURL     https://github.com/LexKreyn/KinopoiskFree/issues
 // ==/UserScript==
+
